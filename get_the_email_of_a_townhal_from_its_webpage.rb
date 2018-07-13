24519e5126c4f9d35f28e72820d67220db449fab
mathieu chevet <mathieuchevet2@hotmail.fr>
2018-07-13 07:59:59 +0200

    Update get_the_email_of_a_townhal_from_its_webpage.rb

--- get_the_email_of_a_townhal_from_its_webpage.rb
+++ get_the_email_of_a_townhal_from_its_webpage.rb
@@ -6,17 +6,17 @@ require 'open-uri'
 @arr_townhal = []
 @arr_mail = []
 @townhal_mail = []
-
+#methode aui retourne l'adresse mail de la mairie de vaureal
 def get_the_email_of_a_townhal_from_its_webpage(url)   
   page_townhal(url)
 end
-
+#méthode qui retourne le tableau de hash de toutes les villes du val d'oise avec leurs mails
 def get_all_the_urls_of_val_doise_townhalls(url, url2)
   url_townhal(url, url2)
   townhal_mail
   puts @townhal_mail
 end
-
+#méthode qui retourne un tableau avec les url de chaque mairie du val d'oise, un tableau avec leurs nom et qui fait appel à la méthode page_townhal en lui envoyant les url des mairies en parametre
 def url_townhal(url, url2)  
   page = Nokogiri::HTML(open("#{url2}"))
   page.xpath('//a[@class = "lientxt"]').each do |node|
@@ -29,7 +29,7 @@ def url_townhal(url, url2)
     @arr_url << url_full
   end
 end
-
+#méthode qui retourne un tableau de tous les mails des mairies
 def page_townhal(url)
   c =[]
   begin
@@ -42,7 +42,7 @@ def page_townhal(url)
 puts 'erreur'
   end
 end
-
+#méthode qui transforme les tableaux de mail et de nom de mairies en tableau de hash
 def townhal_mail
   size = @arr_mail.size - 1
   for i in 0..size
